80f8cb4731ad5c2577c5bd7ed0464fa63ba542e4
Delainey Ackerman <dack91@gmail.com>
2017-06-15 17:54:23 +0100

    move all Oculus camera setup modifications to NVROculusIntegration and out of Oculus SDK; modify gear camera setup for stereo video in progress

--- NewtonVR/Gear/NVRGearIntegration.cs
+++ NewtonVR/Gear/NVRGearIntegration.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿    using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
@@ -85,6 +85,104 @@ namespace NewtonVR
     {
         public override void Initialize(NVRPlayer player)
         {
+            //// Start Integration for Holo with NVR //////
+            NVRPlayer Player = player;
+            Debug.Log("init gear");
+
+            //OVRManager manager = Player.gameObject.AddComponent<OVRManager>();
+            //manager.trackingOriginType = OVRManager.TrackingOrigin.EyeLevel;
+
+            //OVRCameraRig rig = Player.gameObject.AddComponent<OVRCameraRig>();
+            //rig.usePerEyeCameras = true;
+
+            //NVRHelpers.SetProperty(rig, "trackingSpace", Player.transform, true);
+            //NVRHelpers.SetProperty(rig, "centerEyeAnchor", Player.Head.transform, true);
+            //Camera cam = Player.Head.GetComponent<Camera>();
+            //cam.enabled = false;
+            //Camera leftCam = rig.leftEyeAnchor.GetComponent<Camera>();
+            //leftCam.cullingMask = -1;
+            //leftCam.cullingMask &= ~(1 << LayerMask.NameToLayer("RightEye"));
+            //Camera rightCam = rig.rightEyeAnchor.GetComponent<Camera>();
+            //rightCam.cullingMask = -1;
+            //rightCam.cullingMask &= ~(1 << LayerMask.NameToLayer("LeftEye"));
+            //leftCam.allowHDR = false;
+            //rightCam.allowHDR = false;
+
+            // Integration for two camera setup needed for Microsoft Holo video with NewtonVr
+            Player.Head.gameObject.name = "Head_Left";      // rename original NVR head to avoid confusion in editor
+
+            // Create game object and position it to match NVR existing camera
+            GameObject headRight = new GameObject("Head_Right");
+            headRight.gameObject.tag = "MainCamera";
+            headRight.transform.position = Player.Head.gameObject.transform.position;
+            headRight.transform.rotation = Player.Head.transform.rotation;
+            headRight.transform.parent = Player.Head.transform.parent;
+
+            // Add camera to new game object and set for right VR eye
+            Camera camRight = headRight.AddComponent<Camera>();
+            camRight.stereoTargetEye = StereoTargetEyeMask.Right;
+            camRight.cullingMask = -1;
+            camRight.cullingMask &= ~(1 << LayerMask.NameToLayer("LeftEye"));
+            camRight.farClipPlane = 200f;
+            camRight.nearClipPlane = 0.01f;
+            camRight.allowHDR = false;
+
+            // If original NVR head has a camera component, set the camera for left VR eye
+            Camera camLeft = Player.Head.GetComponent<Camera>();
+            if (camLeft)
+            {
+                camLeft.stereoTargetEye = StereoTargetEyeMask.Left;
+                // Set all layers to 1, then compare with & to change "RightEye" layer to 0 while keeping all others 1
+                // 1111 add 1101 get 1101
+                camLeft.cullingMask = -1;
+                camLeft.cullingMask &= ~(1 << LayerMask.NameToLayer("RightEye"));
+                camLeft.farClipPlane = 200f;
+                camLeft.nearClipPlane = 0.01f;
+            }
+
+            // Copy over image effect components from the main camera tagged with "ArtSetupCamera"
+            // to each target eye camera with reflected component values
+            Component[] cameraComponents = Player.Head.GetComponents(typeof(Component));
+
+            // Copy over each component from the head to the right eye
+            foreach (Component curComponent in cameraComponents)
+            {
+                // Get component type
+                System.Type type = curComponent.GetType();
+
+                // Skip certain components ie left eye already has Camera 
+                // component and scene only needs one AudioListener
+                if (type != typeof(Camera) && type != typeof(Transform) &&
+                    type != typeof(AudioListener))// && type != typeof(VideoPlayTest))
+                {
+                    // Add component to right eye game object
+                    Component copy = headRight.AddComponent(type);
+                    // Save active status of component from head
+                    bool isActive = ((Behaviour)Player.Head.GetComponent(type)).enabled;
+
+                    // Reflect all component values from head to right eye
+                    System.Reflection.FieldInfo[] fields = type.GetFields();
+                    foreach (System.Reflection.FieldInfo field in fields)
+                    {
+                        field.SetValue(copy, field.GetValue(curComponent));
+                    }
+
+                    // Set active status of right eye component from status of head
+                    ((Behaviour)headRight.GetComponent(type)).enabled = isActive;
+
+                    if (type == typeof(VideoPlayTest))
+                    {
+                        VideoPlayTest vpt = Player.Head.GetComponent<VideoPlayTest>();
+                        UnityEngine.Video.VideoPlayer vpTop = vpt.LeftEye;
+                        UnityEngine.Video.VideoPlayer vpBot = vpt.RightEye;
+
+                        ((VideoPlayTest)headRight.GetComponent(type)).LeftEye = vpBot;// GameObject.FindGameObjectWithTag("rightPlane").GetComponent<UnityEngine.Video.VideoPlayer>();
+                        ((VideoPlayTest)headRight.GetComponent(type)).RightEye = vpTop;
+                    }
+                }
+            }
+
+            //// End Integration for Holo with NVR //////
         }
 
         public override Vector3 GetPlayspaceBounds()
--- NewtonVR/Oculus/NVROculusIntegration.cs
+++ NewtonVR/Oculus/NVROculusIntegration.cs
@@ -60,6 +60,7 @@ namespace NewtonVR
             OVRManager manager = Player.gameObject.AddComponent<OVRManager>();
             manager.trackingOriginType = OVRManager.TrackingOrigin.FloorLevel;
 
+            // User OVR rig for two camera, per eye setup
             OVRCameraRig rig = Player.gameObject.AddComponent<OVRCameraRig>();
             rig.usePerEyeCameras = true;
 
@@ -69,6 +70,108 @@ namespace NewtonVR
             NVRHelpers.SetProperty(rig, "centerEyeAnchor", Player.Head.transform, true);
 
             Player.gameObject.SetActive(true);
+
+            // Initialize Left and Right eye cameras with components and values matching original head camera
+            // This allows image effects, scripts, etc. set up and tested on the main camera to copy
+            // over maintaining values to two camera setup
+
+            // Reference to the NVR Head game object with desired camera components and values
+            GameObject originalHead = Player.Head.gameObject;
+
+            // Reference to left and right eye cameras created by OVR rig setup 
+            Camera _leftEyeCamera = rig.leftEyeAnchor.GetComponent<Camera>();
+            Camera _rightEyeCamera = rig.rightEyeAnchor.GetComponent<Camera>();
+
+            if (originalHead != null)
+            {
+                ///////////////////// LEFT EYE CAMERA SETUP //////////////////////////////
+
+                // Adjust clipping and target eye for stereo camera
+                _leftEyeCamera.stereoTargetEye = StereoTargetEyeMask.Left;
+                _leftEyeCamera.farClipPlane = 200f;
+                _leftEyeCamera.nearClipPlane = 0.01f;
+
+                // Set all layers to 1, then compare with & to change "RightEye" layer to 0 while keeping all others 1
+                // 1111 add 1101 get 1101
+                _leftEyeCamera.cullingMask = -1;
+                _leftEyeCamera.cullingMask &= ~(1 << LayerMask.NameToLayer("RightEye"));
+
+                // Copy over image effect components from the main camera tagged with "ArtSetupCamera"
+                // to each target eye camera with reflected component values
+                Component[] cameraComponents = originalHead.GetComponents(typeof(Component));
+
+                // Copy over each component from the head to the right eye
+                foreach (Component curComponent in cameraComponents)
+                {
+                    // Get component type
+                    System.Type type = curComponent.GetType();
+
+                    // Skip certain components ie left eye already has Camera 
+                    // component and scene only needs one AudioListener
+                    if (type != typeof(Camera) && type != typeof(Transform) &&
+                        type != typeof(AudioListener))
+                    {
+                        // Add component to right eye game object
+                        Component copy = rig.leftEyeAnchor.gameObject.AddComponent(type);
+                        // Save active status of component from head
+                        bool isActive = ((Behaviour)originalHead.GetComponent(type)).enabled;
+
+                        // Reflect all component values from head to right eye
+                        System.Reflection.FieldInfo[] fields = type.GetFields();
+                        foreach (System.Reflection.FieldInfo field in fields)
+                        {
+                            field.SetValue(copy, field.GetValue(curComponent));
+                        }
+
+                        // Set active status of right eye component from status of head
+                        ((Behaviour) rig.leftEyeAnchor.GetComponent(type)).enabled = isActive;
+                    }
+                }
+
+                ///////////////////// RIGHT EYE CAMERA SETUP //////////////////////////////
+
+                // Adjust clipping and target eye for stereo camera
+                _rightEyeCamera.stereoTargetEye = StereoTargetEyeMask.Right;
+                _rightEyeCamera.farClipPlane = 200f;
+                _rightEyeCamera.nearClipPlane = 0.01f;
+
+                // Set all layers to 1, then compare with & to change "LeftEye" layer to 0 while keeping all others 1
+                // 1111 add 1101 get 1101
+                _rightEyeCamera.cullingMask = -1;
+                _rightEyeCamera.cullingMask &= ~(1 << LayerMask.NameToLayer("LeftEye"));
+
+                // Copy over image effect components from the main camera tagged with "ArtSetupCamera"
+                // to each target eye camera with reflected component values
+               // Component[] cameraComponents = originalHead.GetComponents(typeof(Component));
+
+                // Copy over each component from the head to the right eye
+                foreach (Component curComponent in cameraComponents)
+                {
+                    // Get component type
+                    System.Type type = curComponent.GetType();
+
+                    // Skip certain components ie right eye already has Camera 
+                    // component and scene only needs one AudioListener
+                    if (type != typeof(Camera) && type != typeof(Transform) &&
+                        type != typeof(AudioListener))
+                    {
+                        // Add component to right eye game object
+                        Component copy = rig.rightEyeAnchor.gameObject.AddComponent(type);
+                        // Save active status of component from head
+                        bool isActive = ((Behaviour)originalHead.GetComponent(type)).enabled;
+
+                        // Reflect all component values from head to right eye
+                        System.Reflection.FieldInfo[] fields = type.GetFields();
+                        foreach (System.Reflection.FieldInfo field in fields)
+                        {
+                            field.SetValue(copy, field.GetValue(curComponent));
+                        }
+
+                        // Set active status of right eye component from status of head
+                        ((Behaviour) rig.rightEyeAnchor.GetComponent(type)).enabled = isActive;
+                    }
+                }
+            }
         }
 
         private Vector3 PlayspaceBounds = Vector3.zero;
